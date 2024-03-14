46413b0a0f166e3178986ab13ea67dcff188aa9b
jasonasmk <kaiseroskilo@gmail.com>
2024-03-14 17:37:48 +0200

    update key (delete after)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  movieDBAPIKey: '11a0f38a3c3fb791c22e33a6669a3104',
+  movieDBAPIKey: 'bc50218d91157b1ba4f142ef7baaa6a0',
   movieDBImageCDNUrl: 'https://image.tmdb.org/t/p/w185',
   movieDBPreviewImageCDNUrl: 'https://image.tmdb.org/t/p/w92/',
   movieDBAvatarImageCDNUrl: 'https://image.tmdb.org/t/p/w45/',
